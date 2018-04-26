965dbc9b96b4dc380e76911c04262838fb81e97d
Haicx98 <35750406+Haicx98@users.noreply.github.com>
2018-04-26 20:45:30 +0700

    Controller
    +CheckAmountController
    +Convert
    +SignIn
    +SignIncontroller
    +SignupController
    +TransactionLogController
    +TransferController
    +WithdrawController

---
+++ InternetBankingTeamNam/Controller/TransactionLogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternetBankingTeamNam.Controller
+{
+    class TransactionLogController
+    {
+        public static void TransactionLog(int id, MySqlCommand cmd, MySqlTransaction trs)
+        {
+            DateTime date;
+            List<Entity.TransactionLog> listLog = ManagerModel.TransactionLog(id, cmd);
+            Console.WriteLine("+------------------------------------------------------------------------------------------------------------+");
+            Console.WriteLine("|  TRANSACTION CODE  |\t TRANSACTION TITLE \t|\t BANK NUMBER \t|\t MONEY \t|\t DATE \t     |");
+            Console.WriteLine("+------------------------------------------------------------------------------------------------------------+");
+            if (listLog.Count != 0)
+            {
+                for (int i = 0; i < listLog.Count; i++)
+                {
+                    Console.WriteLine("|     " + listLog[i].TransactionCode +
+                                      "      |\t     " + listLog[i].TransactionTitle +
+                                      "\t        |   " + listLog[i].BankNumber +
+                                      "   |     " + listLog[i].Money +
+                                      "\t| " + (date = new DateTime(listLog[i].TransactionDate)) + " |");
+                }
+            }
+            Console.WriteLine("+------------------------------------------------------------------------------------------------------------+");
+            MenuContinue.MnContinue(id, cmd, trs);
+        }
+    }
+}
---
+++ InternetBankingTeamNam/Controller/WithdrawController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternetBankingTeamNam.Controller
+{
+    class WithDrawController
+    {
+        public static void WithDraw(int id, MySqlCommand cmd, MySqlTransaction trs)
+        {
+            string message = "";
+            string errorMessage = "";
+            CheckExist ctx = new CheckExist();
+            long balence = ctx.BalenceById(id, cmd);
+
+            string title = "WITHDRAWAL";
+            string verifiCode = Securitys.VerificationCode();
+            string bankNumber = ctx.BankNumberById(id, cmd);
+            string bankCode = bankNumber.Substring(0, 3);
+            string transactionCode = Securitys.TransactionCode(bankCode);
+
+            Console.WriteLine("\n+--------------------------------------------+");
+            DateTime now = DateTime.Now;
+            long updated = now.Ticks;
+            while (true)
+            {
+                Console.Write("| THE MONEY YOU WANT TO WITHDRAW : ");
+                long money = Int64.Parse(Console.ReadLine());
+                bool check = ValidateDataInput.IsValidMoney(id, money, out errorMessage, cmd);
+                if (check)
+                {
+                    Console.WriteLine("| VERIFICATION CODE              : " + verifiCode);
+                    Console.WriteLine("+--------------------------------------------+");
+                    Console.Write("| REENTER VERIFICATION CODE      : ");
+                    string reEnter = Console.ReadLine();
+                    if (reEnter.Equals(verifiCode))
+                    {
+                        Console.WriteLine("+--------------------------------------------+");
+                        Console.WriteLine("|         VERIFICATION CODE MATCHING         |");
+                        Console.WriteLine("+--------------------------------------------+");
+                        Console.WriteLine("|        PERFORMING TRANSACTION . . .        |");
+                        Console.WriteLine("+--------------------------------------------+");
+                        balence -= money;
+                        ManagerModel.MoneyWithDraw(transactionCode, title, bankNumber, money, balence, updated, id, cmd, trs, out message);
+                        Console.WriteLine(message);
+
+                        MenuContinue.MnContinue(id, cmd, trs);
+                        break;
+                    }
+
+                    Console.WriteLine("\n+--------------------------------------------+\n" +
+                                      "|        VERIFICATION CODE IS WRONG !        |\n" +
+                                      "+--------------------------------------------+\n");
+                }
+
+                Console.WriteLine(errorMessage);
+            }
+        }
+    }
+}
