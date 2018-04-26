93fb92b2b9b385509b87da4d42af353fac50434c
Haicx98 <35750406+Haicx98@users.noreply.github.com>
2018-04-26 20:14:47 +0700

    Model
    +Check exist
    +ConnectionHelper
    +ManagerModel
    +SignInModel
    +SignUpModel

--- InternetBankingTeamNam/Model/ConnectionHelper.cs
+++ InternetBankingTeamNam/Model/ConnectionHelper.cs
@@ -22,6 +22,26 @@ namespace InternetBankingTeamNam.Model
             return connection;
         }
 
+        public static MySqlConnection GetConnection1()
+        {
+            if (connection == null)
+            {
+                try
+                {
+                    connection = GetConnectionEntity();
+                    connection.Open();
+                    //Console.WriteLine("Success");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+            }
+
+            return connection;
+        }
+
         public static void CloseConnection()
         {
             if (connection != null)
@@ -32,6 +52,25 @@ namespace InternetBankingTeamNam.Model
                 //Console.WriteLine("Close success");
             }
 
+        }
+
+        private static MySqlConnection GetConnectionEntity()
+        {
+            string server = "localhost";
+            int port = 3306;
+            string database = "internetbanking";
+            string username = "root";
+            string passwrod = "";
+
+            return GetConnectionEntity(server, port, database, username, passwrod);
+        }
+
+        private static MySqlConnection GetConnectionEntity(string server, int port, string database, string username,
+            string password)
+        {
+            string ConnectString = "Server = " + server + ";Database = " + database + ";port = " + port + ";username = " + username + ";password = " + password + ";CharSet=utf8;";
+            MySqlConnection connection = new MySqlConnection(ConnectString);
+            return connection;
         }
     }
 }
---
+++ InternetBankingTeamNam/Model/SignInModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InternetBankingTeamNam.Model
+{
+    class SignInModel
+    {
+        public static Account SignIn(int ids, string inputUser, string inputPass, MySqlCommand cmd)
+        {
+            string username = "";
+            string password = "";
+
+            string signIn = "SELECT username, password FROM accounts WHERE id =" + ids;
+            cmd.CommandText = signIn;
+            cmd.CommandType = CommandType.Text;
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                username = reader.GetString("username");
+                password = reader.GetString("password");
+            }
+            reader.Close();
+            Account acc = new Account(username, password);
+
+            return acc;
+        }
+    }
+}
